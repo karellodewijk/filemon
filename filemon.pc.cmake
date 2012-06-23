c5b435adc49247246819d425146e8c8552491477
Karel-Lodewijk Verdonck <karel.lodewijk@gmail.com>
2012-06-24 00:48:43 +0200

    Added pkg_config blueprint

---
+++ filemon.pc.cmake
@@ -0,0 +1,10 @@
+prefix=${EXEC_INSTALL_PREFIX}
+exec_prefix=${BIN_INSTALL_DIR}
+libdir=${LIB_INSTALL_DIR}
+includedir=${INCLUDE_INSTALL_DIR}/filemon
+
+Name: filemon
+Description: A file monitoring library
+Version: ${filemon_VERSION}
+Libs: -lfilemon
+Cflags: -I${INCLUDE_INSTALL_DIR}/filemon
